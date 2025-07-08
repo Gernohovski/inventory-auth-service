1d5d98e96bf8319f3cadc210578165e6e820ec91
Rodrigo Gernohovski <gernohovskirodrigo@gmail.com>
2025-07-07 21:15:31 -0300

    initial commit

---
+++ src/main/java/br/com/fatec/mogi/inventory_auth_service/InventoryAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.mogi.inventory_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/mogi/inventory_auth_service/InventoryAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.mogi.inventory_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
