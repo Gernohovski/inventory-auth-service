f342c12b389db5706d3d48def75b10bbdfd985b7
Rodrigo Gernohovski <gernohovskirodrigo@gmail.com>
2025-07-14 20:56:39 -0300

    feat: Adiciona fluxo de alteração de senha do usuário

--- src/main/java/br/com/fatec/mogi/inventory_auth_service/service/UsuarioService.java
+++ src/main/java/br/com/fatec/mogi/inventory_auth_service/service/UsuarioService.java
@@ -1,10 +1,7 @@
 package br.com.fatec.mogi.inventory_auth_service.service;
 
 import br.com.fatec.mogi.inventory_auth_service.domain.model.Usuario;
-import br.com.fatec.mogi.inventory_auth_service.web.dto.request.CadastrarUsuarioRequestDTO;
-import br.com.fatec.mogi.inventory_auth_service.web.dto.request.ConfirmarCadastroUsuarioRequestDTO;
-import br.com.fatec.mogi.inventory_auth_service.web.dto.request.LoginRequestDTO;
-import br.com.fatec.mogi.inventory_auth_service.web.dto.request.SolicitarResetSenhaRequestDTO;
+import br.com.fatec.mogi.inventory_auth_service.web.dto.request.*;
 import br.com.fatec.mogi.inventory_auth_service.web.dto.response.LoginResponseDTO;
 
 public interface UsuarioService {
@@ -16,5 +13,7 @@ public interface UsuarioService {
 	LoginResponseDTO login(LoginRequestDTO dto);
 
 	boolean solicitarResetSenha(SolicitarResetSenhaRequestDTO dto);
+
+	void alterarSenha(AlterarSenhaRequestDTO dto);
 
 }
--- src/main/java/br/com/fatec/mogi/inventory_auth_service/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/com/fatec/mogi/inventory_auth_service/service/impl/UsuarioServiceImpl.java
@@ -1,18 +1,18 @@
 package br.com.fatec.mogi.inventory_auth_service.service.impl;
 
-import br.com.fatec.mogi.inventory_auth_service.domain.exception.EmailJaUtilizadoException;
-import br.com.fatec.mogi.inventory_auth_service.domain.exception.FuncaoNaoEncontrada;
-import br.com.fatec.mogi.inventory_auth_service.domain.exception.LoginInvalidoException;
-import br.com.fatec.mogi.inventory_auth_service.domain.exception.UsuarioNaoEncontradoException;
+import br.com.fatec.mogi.inventory_auth_service.domain.enums.TipoCache;
+import br.com.fatec.mogi.inventory_auth_service.domain.exception.*;
 import br.com.fatec.mogi.inventory_auth_service.domain.model.Funcao;
 import br.com.fatec.mogi.inventory_auth_service.domain.model.Usuario;
 import br.com.fatec.mogi.inventory_auth_service.domain.model.UsuarioFuncao;
 import br.com.fatec.mogi.inventory_auth_service.domain.model.valueObjects.Email;
+import br.com.fatec.mogi.inventory_auth_service.domain.model.valueObjects.Senha;
 import br.com.fatec.mogi.inventory_auth_service.repository.FuncaoRepository;
 import br.com.fatec.mogi.inventory_auth_service.repository.UsuarioFuncaoRepository;
 import br.com.fatec.mogi.inventory_auth_service.repository.UsuarioRepository;
 import br.com.fatec.mogi.inventory_auth_service.service.AutenticacaoService;
 import br.com.fatec.mogi.inventory_auth_service.service.EmailService;
+import br.com.fatec.mogi.inventory_auth_service.service.RedisService;
 import br.com.fatec.mogi.inventory_auth_service.service.UsuarioService;
 import br.com.fatec.mogi.inventory_auth_service.web.dto.request.*;
 import br.com.fatec.mogi.inventory_auth_service.web.dto.response.LoginResponseDTO;
@@ -20,6 +20,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -34,6 +36,8 @@ public class UsuarioServiceImpl implements UsuarioService {
 	private final FuncaoRepository funcaoRepository;
 
 	private final UsuarioFuncaoRepository usuarioFuncaoRepository;
+
+	private final RedisService redisService;
 
 	@Override
 	@Transactional
@@ -75,6 +79,16 @@ public class UsuarioServiceImpl implements UsuarioService {
 		var usuario = usuarioRepository.findByEmail(new Email(dto.getEmail()))
 				.orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado."));
 		return emailService.enviarEmailResetSenha(usuario);
+	}
+
+	@Override
+	@Transactional
+	public void alterarSenha(AlterarSenhaRequestDTO dto) {
+		var chave = dto.getEmail().concat(dto.getCodigo());
+		var usuario = (Usuario) redisService.buscar(TipoCache.CODIGO_RESET_SENHA, chave);
+		Optional.ofNullable(usuario).orElseThrow(SolicitacaoExpiradaExpcetion::new);
+		usuario.setSenha(new Senha(dto.getNovaSenha()));
+		usuarioRepository.save(usuario);
 	}
 
 }
