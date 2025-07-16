06f7edb0c4c59698f32491b0c9cfbd1474f3973a
Rodrigo Gernohovski <gernohovskirodrigo@gmail.com>
2025-07-15 21:01:54 -0300

    feat: Adiciona endpoint de logout do usuário

--- src/main/java/br/com/fatec/mogi/inventory_auth_service/web/AutenticacaoController.java
+++ src/main/java/br/com/fatec/mogi/inventory_auth_service/web/AutenticacaoController.java
@@ -2,8 +2,11 @@ package br.com.fatec.mogi.inventory_auth_service.web;
 
 import br.com.fatec.mogi.inventory_auth_service.service.AutenticacaoService;
 import br.com.fatec.mogi.inventory_auth_service.service.UsuarioService;
+import br.com.fatec.mogi.inventory_auth_service.web.dto.request.AutorizarUsuarioRequestDTO;
 import br.com.fatec.mogi.inventory_auth_service.web.dto.request.LoginRequestDTO;
+import br.com.fatec.mogi.inventory_auth_service.web.dto.request.LogoutRequestDTO;
 import br.com.fatec.mogi.inventory_auth_service.web.dto.request.RefreshTokenRequestDTO;
+import br.com.fatec.mogi.inventory_auth_service.web.dto.response.AutorizarUsuarioResponseDTO;
 import br.com.fatec.mogi.inventory_auth_service.web.dto.response.LoginResponseDTO;
 import br.com.fatec.mogi.inventory_auth_service.web.dto.response.RefreshTokenResponseDTO;
 import org.springframework.http.HttpStatus;
@@ -24,6 +27,13 @@ public record AutenticacaoController(UsuarioService usuarioService, Autenticacao
 	public ResponseEntity<RefreshTokenResponseDTO> refresh(@RequestBody RefreshTokenRequestDTO dto) {
 		var tokensDto = autenticacaoService.gerarAutenticacao(dto);
 		return ResponseEntity.status(HttpStatus.OK).body(tokensDto);
+	}
+
+	@PutMapping("/logout")
+	public ResponseEntity<?> logout(@RequestHeader("X-REFRESH-TOKEN") String refreshToken,
+																 @RequestHeader("X-ACCESS-TOKEN") String accessToken) {
+		autenticacaoService.logout(new LogoutRequestDTO(accessToken, refreshToken));
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 }
